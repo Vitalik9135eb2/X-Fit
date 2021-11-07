7f156037fc6038362ef750614adbf316a08f2121
unknown <05vp94@mail.ru>
2021-11-07 22:23:23 +0300

    header

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,50 @@
-$(function(){
-    
+
+const tabsServisesBtn = document.querySelectorAll('.services__nav-item');
+const contentServises = document.querySelectorAll('.services__content');
+
+
+tabsServisesBtn.forEach( function (item) {
+    item.addEventListener('click', function () {
+        
+
+    })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelectorAll('.services__nav-link').forEach( (item) => 
+//     item.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         const id = e.target.getAttribute('href').replace('#', '')
+
+//         document.querySelectorAll('.services__nav-link').forEach(
+//             (child) => child.classList.remove('active')
+//         );
+
+//         document.querySelectorAll('.services__content').forEach(
+//             (child) => child.classList.remove('active')
+//         );
+
+//         item.classList.add('active');
+        
+//     })
+
+// );
+
+
+// document.querySelector('.services__nav-link').click();
+
