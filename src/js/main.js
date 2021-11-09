ecb62e50a699992867bb4b02150a6b4c92dec087
unknown <05vp94@mail.ru>
2021-11-09 10:26:08 +0300

    services

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,34 @@
 
-const tabsServisesBtn = document.querySelectorAll('.services__nav-item');
+const tabsServises = document.querySelectorAll('.services__nav-item');
 const contentServises = document.querySelectorAll('.services__content');
 
 
-tabsServisesBtn.forEach( function (item) {
+tabsServises.forEach( function (item) {
     item.addEventListener('click', function () {
-        
 
+        let tabId = item.getAttribute('data-tab');
+        let curentTab = document.querySelector(tabId);
+
+
+        tabsServises.forEach(function (item){
+            item.classList.remove('active')
+            item.childNodes[1].classList.remove('active');
+            item.childNodes[3].classList.remove('active')
+        });
+
+        contentServises.forEach(function (item){
+            item.classList.remove('active')
+        });
+
+        item.classList.add('active');
+        item.childNodes[1].classList.add('active');
+        item.childNodes[3].classList.add('active');
+
+        curentTab.classList.add('active');
     })
-})
+});
+
+document.querySelector('.services__nav-item').click();
 
 
 
