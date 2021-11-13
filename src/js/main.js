95236c896070f6d9523a14085cbca77774a6515e
unknown <05vp94@mail.ru>
2021-11-13 23:12:17 +0300

    block info

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 
+
+// Tabs block "Servises"
+
 const tabsServises = document.querySelectorAll('.services__nav-item');
 const contentServises = document.querySelectorAll('.services__content');
 
@@ -32,9 +35,30 @@ document.querySelector('.services__nav-item').click();
 
 
 
+// Tabs block "Card"
+
+const tabsCard = document.querySelectorAll('.card__list-item');
+const contentCard = document.querySelectorAll('.card__info');
 
 
-
+    tabsCard.forEach(function (item){
+        item.addEventListener('click', function (){
+            let tabId = item.getAttribute('data-tab');
+            let curentTab = document.querySelector(tabId);
+    
+            tabsCard.forEach(function (item){
+                item.classList.remove('active')
+            });
+            contentCard.forEach(function (item){
+                item.classList.remove('active')
+            });
+    
+            item.classList.add('active');
+    
+            curentTab.classList.add('active');
+        })
+    })
+    document.querySelector('.card__list-item').click();
 
 
 
