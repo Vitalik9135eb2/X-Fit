8e35010a991c91c543d5c3079a2048e95e1dd6c5
unknown <05vp94@mail.ru>
2021-11-13 17:17:50 +0300

    block card

--- src/js/main.js
+++ src/js/main.js
@@ -45,26 +45,3 @@ document.querySelector('.services__nav-item').click();
 
 
 
-
-// document.querySelectorAll('.services__nav-link').forEach( (item) => 
-//     item.addEventListener('click', function (e) {
-//         e.preventDefault();
-//         const id = e.target.getAttribute('href').replace('#', '')
-
-//         document.querySelectorAll('.services__nav-link').forEach(
-//             (child) => child.classList.remove('active')
-//         );
-
-//         document.querySelectorAll('.services__content').forEach(
-//             (child) => child.classList.remove('active')
-//         );
-
-//         item.classList.add('active');
-        
-//     })
-
-// );
-
-
-// document.querySelector('.services__nav-link').click();
-
